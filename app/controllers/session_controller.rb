cfc8c6e29c36264ada3568639dceead9b37d437f
Everaldo Gomes <everaldo.gomes@gmail.com>
2017-02-18 03:41:29 +0000

    By pass rate limit - limit increased ten times

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -158,8 +158,8 @@ class SessionController < ApplicationController
       return
     end
 
-    RateLimiter.new(nil, "login-hr-#{request.remote_ip}", 30, 1.hour).performed!
-    RateLimiter.new(nil, "login-min-#{request.remote_ip}", 6, 1.minute).performed!
+    RateLimiter.new(nil, "login-hr-#{request.remote_ip}", 300, 1.hour).performed!
+    RateLimiter.new(nil, "login-min-#{request.remote_ip}", 60, 1.minute).performed!
 
     params.require(:login)
     params.require(:password)
